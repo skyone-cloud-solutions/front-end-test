2af71b9e49c083780f2ce7b47c5f582c468bd71f
mariana <mari.marques26@gmail.com>
2018-07-26 00:54:00 -0300

    versão final

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// fazer um bag que quando clicar, expandir menu 
+
+// ou seja
+
+// ao clicar para expandir, aparecer <p> 
+
+// else esconde <p> 
+
+// tipo isso, se inspira no da debora pq ela fez umas coisas de 'se aparecer tal coisa, faz tal coisa'
+
